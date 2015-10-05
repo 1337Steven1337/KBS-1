2fa48acdda8149183286228d4d2ce968d03d0f37
Erwin Kok <s1079663@student.windesheim.nl>
2015-10-05 09:42:17 +0200

    gay

--- TheHunt/TheHunt/Program.cs
+++ TheHunt/TheHunt/Program.cs
@@ -17,7 +17,8 @@ namespace TheHunt
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //erwin kok
+            //erwin kok 
+            //Jullie zijn allemaal homo's
         }
     }
 }
