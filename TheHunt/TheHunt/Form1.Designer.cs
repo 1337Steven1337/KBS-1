ba8e37093805315b85ff9ab86932ad48f66ac1b2
Steven <coolesteven_8@hotmail.com>
2015-10-05 11:51:00 +0200

    startscherm met knopjes

--- TheHunt/TheHunt/Form1.Designer.cs
+++ TheHunt/TheHunt/Form1.Designer.cs
@@ -30,27 +30,72 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1072, 599);
+            this.button1.Location = new System.Drawing.Point(514, 127);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(415, 106);
+            this.button1.Size = new System.Drawing.Size(300, 75);
             this.button1.TabIndex = 0;
             this.button1.Text = "Start Game";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.btn_StartGameClick);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(514, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(300, 75);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Maak level";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(514, 323);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(300, 75);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Highscores";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(514, 437);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 75);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Options";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(689, 437);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 75);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Exit";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(2528, 1310);
+            this.ClientSize = new System.Drawing.Size(1258, 664);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -62,6 +107,10 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
