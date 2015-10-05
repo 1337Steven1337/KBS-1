78fd02b139f90b3e67a41c315f29d3b7a7654e68
Johan <johan_luiken@hotmail.com>
2015-10-05 09:48:01 +0200

    MVC

--- TheHunt/TheHunt/Program.cs
+++ TheHunt/TheHunt/Program.cs
@@ -11,9 +11,10 @@ namespace TheHunt
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
+        [STAThread] 
         static void Main()
-        {
+        { 
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
