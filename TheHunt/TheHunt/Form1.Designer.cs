404f3b8bc77952b0aaba7645ec20c1d0df78184a
alexander ineke <alexander.ineke@hotmail.com>
2015-10-05 12:36:11 +0200

    niks

--- TheHunt/TheHunt/Form1.Designer.cs
+++ TheHunt/TheHunt/Form1.Designer.cs
@@ -38,10 +38,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(514, 127);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(457, 102);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(300, 75);
+            this.button1.Size = new System.Drawing.Size(267, 60);
             this.button1.TabIndex = 0;
             this.button1.Text = "Start Game";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,53 +49,57 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(514, 225);
+            this.button2.Location = new System.Drawing.Point(457, 180);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(300, 75);
+            this.button2.Size = new System.Drawing.Size(267, 60);
             this.button2.TabIndex = 1;
             this.button2.Text = "Maak level";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(514, 323);
+            this.button3.Location = new System.Drawing.Point(457, 258);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(300, 75);
+            this.button3.Size = new System.Drawing.Size(267, 60);
             this.button3.TabIndex = 2;
             this.button3.Text = "Highscores";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(514, 437);
+            this.button4.Location = new System.Drawing.Point(457, 350);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(125, 75);
+            this.button4.Size = new System.Drawing.Size(111, 60);
             this.button4.TabIndex = 3;
             this.button4.Text = "Options";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(689, 437);
+            this.button5.Location = new System.Drawing.Point(612, 350);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(125, 75);
+            this.button5.Size = new System.Drawing.Size(111, 60);
             this.button5.TabIndex = 4;
             this.button5.Text = "Exit";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1258, 664);
+            this.ClientSize = new System.Drawing.Size(1118, 531);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
