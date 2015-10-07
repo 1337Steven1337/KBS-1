ee60e70915f88c15e9edcae8fb04b11aa85d7e9b
Steven <coolesteven_8@hotmail.com>
2015-10-07 13:14:02 +0200

    1313

--- TheHunt/TheHunt/Form1.Designer.cs
+++ TheHunt/TheHunt/Form1.Designer.cs
@@ -36,17 +36,25 @@ namespace TheHunt
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.buttonSndEf = new System.Windows.Forms.Button();
-            this.buttonSndMu = new System.Windows.Forms.Button();
             this.buttonFuSc = new System.Windows.Forms.Button();
             this.buttonContr = new System.Windows.Forms.Button();
-            this.buttonMenu = new System.Windows.Forms.Button();
             this.labelOptionsHeader = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -70,6 +78,7 @@ namespace TheHunt
             this.pictureBox2.Size = new System.Drawing.Size(614, 150);
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.speelKlikGeluid);
             // 
             // pictureBox3
             // 
@@ -80,7 +89,7 @@ namespace TheHunt
             this.pictureBox3.Size = new System.Drawing.Size(614, 152);
             this.pictureBox3.TabIndex = 7;
             this.pictureBox3.TabStop = false;
-            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_click);
+            this.pictureBox3.Click += new System.EventHandler(this.speelKlikGeluid);
             // 
             // pictureBox4
             // 
@@ -102,35 +111,12 @@ namespace TheHunt
             this.pictureBox5.Size = new System.Drawing.Size(614, 146);
             this.pictureBox5.TabIndex = 9;
             this.pictureBox5.TabStop = false;
-            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
-            // 
-            // buttonSndEf
-            // 
-            this.buttonSndEf.Location = new System.Drawing.Point(66, 210);
-            this.buttonSndEf.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.buttonSndEf.Name = "buttonSndEf";
-            this.buttonSndEf.Size = new System.Drawing.Size(169, 50);
-            this.buttonSndEf.TabIndex = 11;
-            this.buttonSndEf.Text = "Soundeffects: On";
-            this.buttonSndEf.UseVisualStyleBackColor = true;
-            this.buttonSndEf.Visible = false;
-            this.buttonSndEf.Click += new System.EventHandler(this.buttonSndEf_Click);
-            // 
-            // buttonSndMu
-            // 
-            this.buttonSndMu.Location = new System.Drawing.Point(66, 268);
-            this.buttonSndMu.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.buttonSndMu.Name = "buttonSndMu";
-            this.buttonSndMu.Size = new System.Drawing.Size(169, 50);
-            this.buttonSndMu.TabIndex = 12;
-            this.buttonSndMu.Text = "Music: On";
-            this.buttonSndMu.UseVisualStyleBackColor = true;
-            this.buttonSndMu.Visible = false;
-            this.buttonSndMu.Click += new System.EventHandler(this.buttonSndMu_Click);
+            this.pictureBox5.Click += new System.EventHandler(this.Afsluiten);
             // 
             // buttonFuSc
             // 
-            this.buttonFuSc.Location = new System.Drawing.Point(66, 325);
+            this.buttonFuSc.AutoSize = true;
+            this.buttonFuSc.Location = new System.Drawing.Point(101, 504);
             this.buttonFuSc.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonFuSc.Name = "buttonFuSc";
             this.buttonFuSc.Size = new System.Drawing.Size(169, 50);
@@ -142,7 +128,8 @@ namespace TheHunt
             // 
             // buttonContr
             // 
-            this.buttonContr.Location = new System.Drawing.Point(66, 382);
+            this.buttonContr.AutoSize = true;
+            this.buttonContr.Location = new System.Drawing.Point(389, 504);
             this.buttonContr.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonContr.Name = "buttonContr";
             this.buttonContr.Size = new System.Drawing.Size(169, 50);
@@ -152,43 +139,115 @@ namespace TheHunt
             this.buttonContr.Visible = false;
             this.buttonContr.Click += new System.EventHandler(this.buttonContr_Click);
             // 
-            // buttonMenu
-            // 
-            this.buttonMenu.Location = new System.Drawing.Point(66, 440);
-            this.buttonMenu.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.buttonMenu.Name = "buttonMenu";
-            this.buttonMenu.Size = new System.Drawing.Size(169, 50);
-            this.buttonMenu.TabIndex = 15;
-            this.buttonMenu.Text = "Back To Menu";
-            this.buttonMenu.UseVisualStyleBackColor = true;
-            this.buttonMenu.Visible = false;
-            this.buttonMenu.Click += new System.EventHandler(this.buttonMenu_Click);
-            // 
             // labelOptionsHeader
             // 
             this.labelOptionsHeader.AutoSize = true;
             this.labelOptionsHeader.BackColor = System.Drawing.Color.Transparent;
-            this.labelOptionsHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOptionsHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 36.2F, System.Drawing.FontStyle.Bold);
             this.labelOptionsHeader.ForeColor = System.Drawing.Color.White;
-            this.labelOptionsHeader.Location = new System.Drawing.Point(84, 109);
+            this.labelOptionsHeader.Location = new System.Drawing.Point(33, 22);
             this.labelOptionsHeader.Name = "labelOptionsHeader";
-            this.labelOptionsHeader.Size = new System.Drawing.Size(137, 38);
+            this.labelOptionsHeader.Size = new System.Drawing.Size(296, 83);
             this.labelOptionsHeader.TabIndex = 17;
             this.labelOptionsHeader.Text = "Options";
+            this.labelOptionsHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.labelOptionsHeader.Visible = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(357, 191);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(300, 69);
+            this.trackBar1.TabIndex = 18;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Visible = false;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.trackBar3);
+            this.panel1.Controls.Add(this.trackBar2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.labelOptionsHeader);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.buttonContr);
+            this.panel1.Controls.Add(this.buttonFuSc);
+            this.panel1.Location = new System.Drawing.Point(59, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1297, 630);
+            this.panel1.TabIndex = 19;
+            this.panel1.Visible = false;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(357, 373);
+            this.trackBar3.Maximum = 100;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(300, 69);
+            this.trackBar3.TabIndex = 23;
+            this.trackBar3.Value = 50;
+            this.trackBar3.Visible = false;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(357, 282);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(300, 69);
+            this.trackBar2.TabIndex = 22;
+            this.trackBar2.Value = 50;
+            this.trackBar2.Visible = false;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(47, 373);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(232, 37);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Effects Volume";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(43, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 37);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Music Volume:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(47, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 37);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Master Volume:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1440, 900);
-            this.Controls.Add(this.labelOptionsHeader);
-            this.Controls.Add(this.buttonMenu);
-            this.Controls.Add(this.buttonContr);
-            this.Controls.Add(this.buttonFuSc);
-            this.Controls.Add(this.buttonSndMu);
-            this.Controls.Add(this.buttonSndEf);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
@@ -206,25 +265,15 @@ namespace TheHunt
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
-        private void buttonMenu_Click(object sender, EventArgs e)
-        {
-            buttonSndEf.Visible = false;
-            buttonSndMu.Visible = false;
-            buttonFuSc.Visible = false;
-            buttonContr.Visible = false;
-            buttonMenu.Visible = false;
-            labelOptionsHeader.Visible = false;
-            pictureBox1.Visible = true;
-            pictureBox2.Visible = true;
-            pictureBox3.Visible = true;
-            pictureBox4.Visible = true;
-            pictureBox5.Visible = true;
-        }
 
         private void buttonContr_Click(object sender, EventArgs e)
         {
@@ -235,37 +284,17 @@ namespace TheHunt
         {
             if (buttonFuSc.Text == "Full Screen: On")
             {
-                buttonFuSc.Text = "Full Screen: Off";
+                GoFullscreen(false);
             }
             else if (buttonFuSc.Text == "Full Screen: Off")
             {
-                buttonFuSc.Text = "Full Screen: On";
+                GoFullscreen(true);
             }
         }
 
-        private void buttonSndMu_Click(object sender, EventArgs e)
-        {
-            if (buttonSndMu.Text == "Music: On")
-            {
-                buttonSndMu.Text = "Music: Off";
-            }
-            else if (buttonSndMu.Text == "Music: Off")
-            {
-                buttonSndMu.Text = "Music: On";
-            }
-        }
 
-        private void buttonSndEf_Click(object sender, EventArgs e)
-        {
-            if (buttonSndEf.Text == "Soundeffects: On")
-            {
-                buttonSndEf.Text = "Soundeffects: Off";
-        }
-            else if (buttonSndEf.Text == "Soundeffects: Off")
-            {
-                buttonSndEf.Text = "Soundeffects: On";
-            }
-        }
+
+
 
 
 
@@ -281,12 +310,16 @@ namespace TheHunt
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.Button buttonSndEf;
-        private System.Windows.Forms.Button buttonSndMu;
         private System.Windows.Forms.Button buttonFuSc;
         private System.Windows.Forms.Button buttonContr;
-        private System.Windows.Forms.Button buttonMenu;
         private System.Windows.Forms.Label labelOptionsHeader;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
