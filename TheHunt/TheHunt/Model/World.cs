8874c5a03e463a177fc5dfb510abae0ea2185178
Remco Schipper <remcoschipper@outlook.com>
2015-10-07 14:35:48 +0200

    Test

---
+++ TheHunt/TheHunt/Model/World.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHunt.Model
+{
+    class World
+    {
+        public List<FieldObject> FieldObjects = new List<FieldObject>();
+        public Player Player = new Player();
+    }
+
+    public struct Position
+    {
+        public int x;
+        public int y;
+    }
+
+    public class Player
+    {
+        public string img;
+        public Position start;
+    }
+}
