ec17bd56faa5d06754fe5dd0e2011cf209f2ee02
Erwin Kok <s1079663@student.windesheim.nl>
2015-10-07 09:56:31 +0200

    raster

--- TheHunt/TheHunt/Map.cs
+++ TheHunt/TheHunt/Map.cs
@@ -12,27 +12,40 @@ namespace TheHunt
 {
     public partial class Map : Form
     {
+
+        public int x = 0;
+        public int y = 0;
+
         public Map()
         {
             InitializeComponent();
-            Paint += new PaintEventHandler(DrawRect);
-            
         }
 
-        public void DrawRect(object sender, PaintEventArgs e)
+        protected override void OnPaint(PaintEventArgs e)
         {
-
-            var screen = Screen.PrimaryScreen.Bounds;
-            var width = screen.Width / 100;
-            var height = screen.Height / 100;
+            //roep base onpaint aan!
+            base.OnPaint(e);
 
 
             Graphics g = e.Graphics;
-            SolidBrush brush = new SolidBrush(Color.Red);
-            Rectangle rect = new Rectangle(10, 10, height, width);
-            g.FillRectangle(brush, rect);
-            //yolo
+            Pen pen = new Pen(Color.Red);
+            for (int x = 0; x < this.Width; x += 20)
+            {
+                Rectangle rect = new Rectangle(x, y, 20, 20);
+                g.DrawRectangle(pen, rect);
+                if (x > this.Width)
+                {
+                    x = 0;
+                }
+                for (int y = 0; y < this.Height; y += 20)
+                {
+                    Rectangle rect2 = new Rectangle(x, y, 20, 20);
+                    g.DrawRectangle(pen, rect2);
+                }
+            }
+            
         }
 
+        
     }
 }
