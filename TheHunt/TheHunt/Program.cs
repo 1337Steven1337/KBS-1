1ce1d34c1c07c5b4ce581cc949b224169d4a1303
Johan <johan_luiken@hotmail.com>
2015-10-08 11:46:08 +0200

    Fx

--- TheHunt/TheHunt/Program.cs
+++ TheHunt/TheHunt/Program.cs
@@ -16,7 +16,7 @@ namespace TheHunt
         {             
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Charactertestform());
+            Application.Run(new Form1());
             //bende
         }
     }
