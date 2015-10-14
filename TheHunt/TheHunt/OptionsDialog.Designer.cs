2f46c4d55af168d0b14bf14905be36cf0b45ca94
GeneralCheesecake <gideon.van.de.weg@gmail.com>
2015-10-14 12:27:56 +0200

    OptionsClass

---
+++ TheHunt/TheHunt/OptionsDialog.Designer.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace TheHunt
+{
+    partial class OptionsDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBarMasterVolume = new System.Windows.Forms.TrackBar();
+            this.trackBarMusicVolume = new System.Windows.Forms.TrackBar();
+            this.trackBarEffectsVolume = new System.Windows.Forms.TrackBar();
+            this.buttonFullScreen = new System.Windows.Forms.Button();
+            this.buttonBackToMenu = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarMasterVolume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarMusicVolume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarEffectsVolume)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBarMasterVolume
+            // 
+            this.trackBarMasterVolume.Location = new System.Drawing.Point(215, 12);
+            this.trackBarMasterVolume.Name = "trackBarMasterVolume";
+            this.trackBarMasterVolume.Size = new System.Drawing.Size(104, 56);
+            this.trackBarMasterVolume.TabIndex = 0;
+            // 
+            // trackBarMusicVolume
+            // 
+            this.trackBarMusicVolume.Location = new System.Drawing.Point(215, 74);
+            this.trackBarMusicVolume.Name = "trackBarMusicVolume";
+            this.trackBarMusicVolume.Size = new System.Drawing.Size(104, 56);
+            this.trackBarMusicVolume.TabIndex = 1;
+            // 
+            // trackBarEffectsVolume
+            // 
+            this.trackBarEffectsVolume.Location = new System.Drawing.Point(215, 136);
+            this.trackBarEffectsVolume.Name = "trackBarEffectsVolume";
+            this.trackBarEffectsVolume.Size = new System.Drawing.Size(104, 56);
+            this.trackBarEffectsVolume.TabIndex = 2;
+            // 
+            // buttonFullScreen
+            // 
+            this.buttonFullScreen.Location = new System.Drawing.Point(215, 198);
+            this.buttonFullScreen.Name = "buttonFullScreen";
+            this.buttonFullScreen.Size = new System.Drawing.Size(150, 30);
+            this.buttonFullScreen.TabIndex = 3;
+            this.buttonFullScreen.Text = "Full Screen: On";
+            this.buttonFullScreen.UseVisualStyleBackColor = true;
+            this.buttonFullScreen.Click += new System.EventHandler(this.buttonFullScreen_Click);
+            // 
+            // buttonBackToMenu
+            // 
+            this.buttonBackToMenu.Location = new System.Drawing.Point(215, 234);
+            this.buttonBackToMenu.Name = "buttonBackToMenu";
+            this.buttonBackToMenu.Size = new System.Drawing.Size(150, 30);
+            this.buttonBackToMenu.TabIndex = 4;
+            this.buttonBackToMenu.Text = "Back To Menu";
+            this.buttonBackToMenu.UseVisualStyleBackColor = true;
+            this.buttonBackToMenu.Click += new System.EventHandler(this.buttonBackToMenu_Click);
+            // 
+            // OptionsDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 300);
+            this.Controls.Add(this.buttonBackToMenu);
+            this.Controls.Add(this.buttonFullScreen);
+            this.Controls.Add(this.trackBarEffectsVolume);
+            this.Controls.Add(this.trackBarMusicVolume);
+            this.Controls.Add(this.trackBarMasterVolume);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "OptionsDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OptionsDialog";
+            this.Load += new System.EventHandler(this.OptionsDialog_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarMasterVolume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarMusicVolume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarEffectsVolume)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBarMasterVolume;
+        private System.Windows.Forms.TrackBar trackBarMusicVolume;
+        private System.Windows.Forms.TrackBar trackBarEffectsVolume;
+        private System.Windows.Forms.Button buttonFullScreen;
+        private System.Windows.Forms.Button buttonBackToMenu;
+    }
+}
