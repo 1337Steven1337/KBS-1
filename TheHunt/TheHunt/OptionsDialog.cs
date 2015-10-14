39b30469d38fdd6bcce9f5a6d10e45f93b2f27f4
GeneralCheesecake <gideon.van.de.weg@gmail.com>
2015-10-14 14:35:38 +0200

    Volume(nogkapot)

--- TheHunt/TheHunt/OptionsDialog.cs
+++ TheHunt/TheHunt/OptionsDialog.cs
@@ -7,16 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media;
+using TheHunt.Controller;
+using TheHunt.Designer;
 
 namespace TheHunt
 { 
+
     public partial class  OptionsDialog : Form
     {
+        MediaPlayer bgm = new MediaPlayer();
+        MediaPlayer klikMP = new MediaPlayer();
+        private Sound sound = null;
+
         private Boolean isClosed = false;
         private Boolean changeFullScreen = false;
          
         public OptionsDialog(Boolean inGame)
-        { 
+        {
+            this.sound = Sound.Instance;
+            //Properties.Sound.Default.PropertyChanged += SoundPropertyChanged;
             InitializeComponent();
             if (inGame)
             {
@@ -24,6 +34,18 @@ namespace TheHunt
             }
             //this.Visible = true;
         }
+
+        //private void SoundPropertyChanged(object sender, PropertyChangedEventArgs e)
+        //{
+        //    if (e.PropertyName == "music")
+        //    {
+        //        bgm.Volume = Properties.Sound.Default.music;
+        //    }
+        //    else if (e.PropertyName == "effects")
+        //    {
+        //        klikMP.Volume = Properties.Sound.Default.effects;
+        //    }
+        //}
 
         private void OptionsDialog_Load(object sender, EventArgs e)
         {
@@ -55,6 +77,24 @@ namespace TheHunt
         public Boolean getChangeFullScreen()
         {
             return changeFullScreen;
+        }
+
+        private void trackBarMasterVolume_Scroll(object sender, EventArgs e)
+        {
+            Properties.Sound.Default.master = MasterTrackBar.Value;
+            Properties.Sound.Default.Save();
+        }
+
+        private void trackBarMusicVolume_Scroll(object sender, EventArgs e)
+        {
+            Properties.Sound.Default.music = MusicTrackBar.Value;
+            Properties.Sound.Default.Save();
+        }
+
+        private void trackBarEffectsVolume_Scroll(object sender, EventArgs e)
+        {
+            Properties.Sound.Default.effects = EffectsTrackbar.Value;
+            Properties.Sound.Default.Save();
         }
 
     }
