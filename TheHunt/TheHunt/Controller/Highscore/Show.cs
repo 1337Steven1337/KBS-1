ffa80c15ef149742698aa869c0e4a22cd86750a0
Remco Schipper <github@remcoschipper.com>
2015-10-15 14:36:46 +0200

    Highscore lay-out

--- TheHunt/TheHunt/Controller/Highscore/Show.cs
+++ TheHunt/TheHunt/Controller/Highscore/Show.cs
@@ -16,8 +16,6 @@ namespace TheHunt.Controller.Highscore
         public Show()
         {
             InitializeComponent();
-
-            //this.MaximumSize = new Size(400, 400);
         }
 
         private void Highscores_Load(object sender, EventArgs e)
@@ -45,6 +43,11 @@ namespace TheHunt.Controller.Highscore
             }
 
             return data;
+        }
+
+        private void buttonBackToMenu_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
