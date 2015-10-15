c476b2f7bde4abb5f97486e84b7f686b2cd495a4
Erwin Kok <s1079663@student.windesheim.nl>
2015-10-15 10:18:53 +0200

    KAPOT

--- TheHunt/TheHunt/Model/World.cs
+++ TheHunt/TheHunt/Model/World.cs
@@ -34,27 +34,13 @@ namespace TheHunt.Model
         public Movement movement;
         public int sizeBreedte = Screen.PrimaryScreen.Bounds.Width / 40;
         public int sizeHoogte = Screen.PrimaryScreen.Bounds.Height / 20;
+        public static Bitmap bitmap = Properties.Resources.brockSprite1;
         public static List<Bitmap> PlayerSprites = new List<Bitmap>();
-        public static Bitmap bitmap;
 
         public Player1()
         {
             PlayerSprites.Add(null);
-            PlayerSprites.Add(Properties.Resources.brockSprite1);
-            PlayerSprites.Add(Properties.Resources.brockSprite2);
-            PlayerSprites.Add(Properties.Resources.brockSprite3);
-            PlayerSprites.Add(Properties.Resources.brockSprite4);
-            PlayerSprites.Add(Properties.Resources.brockSprite5);
-            PlayerSprites.Add(Properties.Resources.brockSprite6);
-            PlayerSprites.Add(Properties.Resources.brockSprite7);
-            PlayerSprites.Add(Properties.Resources.brockSprite8);
-            PlayerSprites.Add(Properties.Resources.brockSprite9);
-            PlayerSprites.Add(Properties.Resources.brockSprite10);
-            PlayerSprites.Add(Properties.Resources.brockSprite11);
-            PlayerSprites.Add(Properties.Resources.brockSprite12);
-
-            bitmap = PlayerSprites[1];
-
+            PlayerSprites.Add(Properties.Resources.Player.brockSprite1);
         }
 
         public void draw(Graphics g,Size screenSize)
